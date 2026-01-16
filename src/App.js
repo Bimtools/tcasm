d156c33b4c389aadfa97e0661eec0d850e634483
Bimtools4.0 <Bimtools4.0@gmail.com>
2026-01-16 09:01:07 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as WorkspaceAPI from "trimble-connect-workspace-api";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  React.useEffect(() => {
+    async function fetchData() {
+      const url = window.location.href;
+      const ifcGuid = url.split("?")[1];
+      if (!ifcGuid) {
+        return;
+      }
+      if (ifcGuid.length !== 22) {
+        return;
+      }
+      const tcapi = await WorkspaceAPI.connect(window.parent);
+      var models;
+      do {
+        models = await tcapi.viewer.getModels();
+      } while (models === undefined || models.length === 0);
+      console.log(ifcGuid);
+      var asm;
+      var modelId;
+      for (const model of models) {
+        await tcapi.viewer.toggleModel(model.id, true, true);
+        var modelObjects;
+        do {
+          modelObjects = await tcapi.viewer.getObjects();
+        } while (modelObjects === undefined || modelObjects.length === 0);
+        const runtimeIds = await tcapi.viewer.convertToObjectRuntimeIds(
+          model.id,
+          [ifcGuid]
+        );
+        console.log(runtimeIds);
+        if (
+          runtimeIds !== undefined &&
+          runtimeIds.length > 0 &&
+          runtimeIds[0] >= 0
+        ) {
+          asm = runtimeIds[0];
+          modelId = model.id;
+          break;
+        } else {
+          await tcapi.viewer.toggleModel(model.id, false);
+        }
+      }
+      console.log(asm);
+      console.log(modelId);
+
+      tcapi.viewer.setSelection({
+        modelObjectIds: [
+          {
+            modelId: modelId,
+            objectRuntimeIds: [asm],
+          },
+        ],
+      });
+      tcapi.viewer.isolateEntities([
+        {
+          modelId: modelId,
+          entityIds: [asm],
+        },
+      ]);
+
+      tcapi.viewer.setCamera({
+        modelObjectIds: [
+          {
+            modelId: modelId,
+            objectRuntimeIds: [asm],
+          },
+        ],
+      });
+    }
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Assembly has been loaded!</p>
       </header>
     </div>
   );
